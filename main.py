cca712556f940e96447903fe0e88294ccc29f542
jibson1536 <153529742+jibson1536@users.noreply.github.com>
2023-12-13 13:23:44 +0100

    i added new file

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 11 16:27:31 2023
+
+@author: admin
+"""
+
+import supportFile
+
+supportFile.saySomething()
+
+print('hello world')
---
+++ supportFile.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 11 16:46:41 2023
+
+@author: admin
+"""
+
+
+def saySomething():
+    print('something')
+
